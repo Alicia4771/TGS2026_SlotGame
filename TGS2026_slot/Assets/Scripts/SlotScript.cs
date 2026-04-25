2990c157e4b33c089610842d12cc1daaff87f8c7
Alicia4771 <rinn1204@icloud.com>
2026-04-26 01:37:33 +0900

    スロットの絵柄の画像のサイズを揃えた＋SlotScriptの作成

--- TGS2026_slot/Assets/Scripts/SlotScript.cs
+++ TGS2026_slot/Assets/Scripts/SlotScript.cs
@@ -1,23 +1,57 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlotScript : MonoBehaviour
 {
-    
-    
+    // スロットの絵柄の画像
+    [SerializeField] private Sprite image_slot_7;
+    [SerializeField] private Sprite image_slot_cherry;
+    [SerializeField] private Sprite image_slot_bell;
+    [SerializeField] private Sprite image_slot_bar;
+    [SerializeField] private Sprite image_slot_replay;
+    [SerializeField] private Sprite image_slot_suika;
+
+    // スロットの絵柄が表示される場所
+    [SerializeField] private SpriteRenderer slot_left;
+    [SerializeField] private SpriteRenderer slot_center;
+    [SerializeField] private SpriteRenderer slot_right;
+
     void Start()
     {
-        
+        if (
+            image_slot_7 == null ||
+            image_slot_cherry == null ||
+            image_slot_bell == null ||
+            image_slot_bar == null ||
+            image_slot_replay == null ||
+            image_slot_suika == null ||
+            slot_left == null ||
+            slot_center == null ||
+            slot_right == null
+        )
+        {
+            Debug.LogError("必要なスロット画像が設定されていません。実行を終了します。");
+
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
         {
             StopSlotLeft();
-        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        } else if (Keyboard.current.digit2Key.wasPressedThisFrame)
         {
             StopSlotCenter();
-        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        } else if (Keyboard.current.digit3Key.wasPressedThisFrame)
         {
             StopSlotRight();
         }
@@ -25,7 +59,7 @@ public class SlotScript : MonoBehaviour
 
     private void StopSlotLeft()
     {
-        // code;
+        slot_left.sprite = image_slot_7;
     }
 
     private void StopSlotCenter()
